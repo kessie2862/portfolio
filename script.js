f81e270ad2f60b8b95d23886bcd23034848fc30b
kessie <kayp3477@gmail.com>
2023-04-10 15:15:40 +0000

    Add functionality to mobile menu

---
+++ script.js
@@ -0,0 +1,40 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('.nav__items');
+const overlay = document.querySelector('.overlay');
+
+// open nav menu
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'inline-block';
+  overlay.classList.remove('hidden');
+
+  // Prevent scrolling when overlay is active
+  document.body.style.overflow = 'hidden';
+});
+
+// close nav menu function
+const closeNavMenu = () => {
+  menu.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+  closeBtn.style.display = 'none';
+  overlay.classList.add('hidden');
+
+  // Enable scrolling when overlay is inactive
+  document.body.style.overflow = 'scroll';
+};
+
+closeBtn.addEventListener('click', () => {
+  closeNavMenu();
+});
+
+// close nav menu when any nav Items is clicked on tablets and mobile
+const navItems = document.querySelectorAll('nav__items li a');
+if (window.innerWidth < 1000) {
+  navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      closeNavMenu();
+    });
+  });
+}
