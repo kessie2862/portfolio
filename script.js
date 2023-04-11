4f47fe90c7a0453c1d4a62bdee9158cfe0799cec
kessie <kayp3477@gmail.com>
2023-04-11 15:55:05 +0000

    Fix linter error

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
-const menu = document.querySelector(".nav__items");
-const overlay = document.querySelector(".overlay");
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('.nav__items');
+const overlay = document.querySelector('.overlay');
 
 // open nav menu
-menuBtn.addEventListener("click", () => {
-  menu.style.display = "block";
-  menuBtn.style.display = "none";
-  closeBtn.style.display = "inline-block";
-  overlay.classList.remove("hidden");
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'inline-block';
+  overlay.classList.remove('hidden');
 
   // Prevent scrolling when overlay is active
-  document.body.style.overflow = "hidden";
+  document.body.style.overflow = 'hidden';
 });
 
 // close nav menu function
 const closeNavMenu = () => {
-  menu.style.display = "none";
-  menuBtn.style.display = "inline-block";
-  closeBtn.style.display = "none";
-  overlay.classList.add("hidden");
+  menu.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+  closeBtn.style.display = 'none';
+  overlay.classList.add('hidden');
 
   // Enable scrolling when overlay is inactive
-  document.body.style.overflow = "scroll";
+  document.body.style.overflow = 'scroll';
 };
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener('click', () => {
   closeNavMenu();
 });
 
 // close nav menu when any nav Items is clicked on tablets and mobile
-const navItems = document.querySelectorAll("nav__items li a");
+const navItems = document.querySelectorAll('nav__items li a');
 if (window.innerWidth < 767) {
   navItems.forEach((item) => {
-    item.addEventListener("click", () => {
+    item.addEventListener('click', () => {
       closeNavMenu();
     });
   });
 
   // hide nav items when clicked
-  menu.addEventListener("click", () => {
-    menu.style.display = "none";
-    document.body.style.overflow = "scroll";
-    overlay.classList.add("hidden");
-    menuBtn.style.display = "block";
+  menu.addEventListener('click', () => {
+    menu.style.display = 'none';
+    document.body.style.overflow = 'scroll';
+    overlay.classList.add('hidden');
+    menuBtn.style.display = 'block';
   });
 }
